4e87d61c7da411be102d7be673a3ef66bdc79378
amitstudydude <amitstudydude@users.noreply.github.com>
2022-09-06 15:50:05 +0000

    merge

--- scripts/autoclick.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install xdotool
-xdotool getmouselocation --shell
-while :; do xdotool mousemove $X $Y click 1 ; sleep 10 ;  done
-
-
-##curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/autoclick.sh | bash
-
-##
--- scripts/runner.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
-mkdir actions-runner && cd actions-runner
-curl -o actions-runner-linux-x64-2.292.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.292.0/actions-runner-linux-x64-2.292.0.tar.gz 
-echo "14839ba9f3da01abdd51d1eae0eb53397736473542a8fae4b7618d05a5af7bb5  actions-runner-linux-x64-2.292.0.tar.gz" | shasum -a 256 -c
-sudo tar xzf ./actions-runner-linux-x64-2.292.0.tar.gz
-printf " \n\n$token\n$token\n\n "| ./config.sh --url https://github.com/$repo --token $token
-./run.sh 
